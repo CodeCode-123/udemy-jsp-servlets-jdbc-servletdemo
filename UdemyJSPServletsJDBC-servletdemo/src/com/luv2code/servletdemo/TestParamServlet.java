4b92675d4f2ce7394d1082a311acd5c2e85c2c43
Shen Tang <stang311@gatech.edu>
2026-02-18 02:08:43 -0500

    Added Servlet Fundamentals

--- UdemyJSPServletsJDBC-servletdemo/src/com/luv2code/servletdemo/TestParamServlet.java
+++ UdemyJSPServletsJDBC-servletdemo/src/com/luv2code/servletdemo/TestParamServlet.java
@@ -3,23 +3,23 @@ package com.luv2code.servletdemo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorldServlet
+ * Servlet implementation class TestParamServlet
  */
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+//@WebServlet("/TestParamServlet")
+public class TestParamServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorldServlet() {
+    public TestParamServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,15 +28,25 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Step 1: set the content type
+		// Step 1: set content type
 		response.setContentType("text/html");
-		// Step 2: get the printwriter
+		// Step 2: get printwriter
 		PrintWriter out = response.getWriter();
-		// Step 3: generate HTML content
+		// Step 3: read configuration params
+		ServletContext context = getServletContext(); // inherit from HttpServlet
+		String maxCartSize = context.getInitParameter("max-shopping-cart-size");
+		String teamName = context.getInitParameter("project-team-name");
+		// READ PER-SERVLET parameter
+		String theGreetingMessage = getInitParameter("greeting");
+		String theServiceLevel = getInitParameter("serviceLevel");
+		// Step 4: generate HTML content
 		out.println("<html><body>");
-		out.println("<h2>Hello World</h2>");
+		out.println("Max cart: " + maxCartSize + "<br><br>");
+		out.println("Team name: " + teamName);
 		out.println("<hr>");
-		out.println("Time on the server is: " + new java.util.Date());
+		out.println("Per Servlet Params<br><br>");
+		out.println("greeting: " + theGreetingMessage + "<br><br>");
+		out.println("serviceLevel: " + theServiceLevel);
 		out.println("</body></html>");
 	}
 
